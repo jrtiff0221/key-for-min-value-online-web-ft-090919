04d02ba63b8d811ecf496206e5c112c0abb06d5a
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-16 05:42:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   smallest_key = nil
   
   name_hash.each do |key, value| 
-    if value <= smallest_value
+    if value < smallest_value
       smallest_key = key
     end
   end
